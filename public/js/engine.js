7fda7d7cd385650e714878a7001c6d94ed998bbc
santi8ago8 <santi8ago8@gmail.com>
2013-09-13 14:13:13 -0300

    initial commit

---
+++ public/js/engine.js
@@ -0,0 +1,31 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: Santiago.Cordoba
+ * Date: 9/13/13
+ * Time: 11:53 AM
+ * To change this template use File | Settings | File Templates.
+ */
+
+var SantiagoApp = angular.module('SantiagoApp', []);
+
+SantiagoApp.config(function ($locationProvider,$routeProvider) {
+    $routeProvider
+        .when('/', {
+            templateUrl: '/jade/home'
+        })
+        .when('/form',{
+            templateUrl: '/jade/form'
+        })
+        .otherwise({redirectTo: '/'})
+});
+
+var controllersSantiagoApp={};
+
+controllersSantiagoApp.navController=function($scope){
+    $scope.select=function(ar){
+        console.log($scope);
+
+    }
+};
+
+SantiagoApp.controller(controllersSantiagoApp);
